b1dc8c3236589aa79e1d8d42973439eff80346bd
Avinash Kushwaha <avinash.kushwaha1501@gmail.com>
2025-07-18 12:04:56 +0530

    Added and fixed schema with imports

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,7 +3,12 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import { middleware } from "./middleware";
 import { JWT_SECRET } from "@repo/backend-common/config";
-import { CreateUserSchema, CreateRoomSchema } from "@repo/common/types";
+import {
+  CreateUserSchema,
+  CreateRoomSchema,
+  SigninSchema,
+} from "@repo/common/types";
+import { prismaClient } from "@repo/db/index";
 
 const app = express();
 
@@ -14,19 +19,36 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signup", (req, res) => {
-  const data = CreateUserSchema.safeParse(req.body);
-  if (!data.success) {
+  const parsedData = CreateUserSchema.safeParse(req.body);
+  if (!parsedData.success) {
     return res.json({ message: "Incorrect Inputs" });
+  } else {
+    prismaClient.user.create({
+      data: {
+        email: parsedData.data.email,
+        password: parsedData.data.password,
+        name: parsedData.data.name,
+      },
+    });
   }
 });
 
 app.post("/signin", middleware, (req, res) => {
-  const userId = 1;
-  const token = jwt.sign({ userId }, JWT_SECRET);
-  res.json(token);
+  const data = SigninSchema.safeParse(req.body);
+  if (!data.success) {
+    return res.json({ message: "Incorrect Inputs" });
+  }
+  // const userId = prismaClient.user;
+  // const token = jwt.sign({ userId }, JWT_SECRET);
+  // res.json(token);
 });
 
-app.post("/create-room", middleware, (req, res) => {});
+app.post("/create-room", middleware, (req, res) => {
+  const data = CreateRoomSchema.safeParse(req.body);
+  if (!data.success) {
+    return res.json({ message: "Incorrect Inputs" });
+  }
+});
 
 app.listen(3000, () => {
   console.log("http-server running at port : 3000");
--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -2,17 +2,16 @@ import { z } from "zod";
 
 export const CreateUserSchema = z.object({
   username: z.string(),
+  email: z.string(),
   password: z.string().min(6).max(12),
   name: z.string(),
 });
-
 
 export const SigninSchema = z.object({
   username: z.string(),
   password: z.string().min(6).max(12),
 });
 
-
 export const CreateRoomSchema = z.object({
-  name: z.string().min(6).max(8)
-})
+  name: z.string().min(6).max(8),
+});
